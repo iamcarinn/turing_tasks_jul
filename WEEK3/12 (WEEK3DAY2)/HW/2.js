c9f2c1da71a8170658d70973fb89ee15ececadbc
iamcarinn <karina825karpenkova@yandex.ru>
2021-08-09 16:43:23 +0300

    HW11

---
+++ WEEK3/12 (WEEK3DAY2)/HW/2.js
@@ -0,0 +1,16 @@
+function cococo() {
+    let a=prompt('Введите число');
+    let b=prompt('Введите ещё число пожалуйста :)');
+
+    if (a>b) {
+        alert(1);
+    } else {
+        if (a<b) {
+            alert(-1);
+        } else {
+            alert(0);
+        }
+    }
+}
+
+cococo()
