5d7fc342b315b1f26aa5083db0eba8a9294a3a22
iamcarinn <karina825karpenkova@yandex.ru>
2021-08-10 18:18:51 +0300

    HW12

---
+++ WEEK3/12 (WEEK3DAY2)/HW/5.js
@@ -0,0 +1,11 @@
+const fib=(n) => {
+    if (typeof n!=="number") return 'Вы ввели не число';
+    if (n===1) return 0;
+    if (n===2) return 0;
+    if (n<=0) {
+        break
+    } else
+
+}
+
+fib(3);
