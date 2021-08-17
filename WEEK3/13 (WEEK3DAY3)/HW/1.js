bb0250978555b8b9222037cdae111926c15cb80a
iamcarinn <karina825karpenkova@yandex.ru>
2021-08-17 15:32:14 +0300

    1

---
+++ WEEK3/13 (WEEK3DAY3)/HW/1.js
@@ -0,0 +1,24 @@
+console.log('Цикл while (...) { *…*}');
+
+let i=0;
+while (i<11) {
+    console.log(i);
+    i++;
+}
+
+///////////////////////////////////////////
+console.log('Цикл do { *...* } while(...)');
+
+let t=0;
+do {
+    console.log(t);
+    t++;
+} while (t<11);
+
+///////////////////////////////////////////
+console.log('Цикл for (...) { *...* }');
+
+for (let x=0;x<11;x++) {
+    console.log(x);
+}
+
