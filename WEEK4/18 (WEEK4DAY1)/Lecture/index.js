41e3cfd8c58b085541be369aed7db99ad1718a0c
iamcarinn <karina825karpenkova@yandex.ru>
2021-11-22 16:44:22 +0300

    18 LC

--- WEEK4/18 (WEEK4DAY1)/Lecture/index.js
+++ WEEK4/18 (WEEK4DAY1)/Lecture/index.js
@@ -40,7 +40,7 @@ console.log(bar(10));*/
 /*ОТЛОЖЕННЫЙ ВЫЗОВ ФУНКЦИИ
 
 Добавить в очередь произвольный код
-ч/з функции JS (setTimeout(() => действие, время в мс (1с=1000мс)) или setinterval), которые позволяют вызвать
+ч/з функции JS (setTimeout(() => действие, время в мс (1с=1000мс)) или setInterval()), которые позволяют вызвать
 фун-цию один раз ч/з определ. интервал времени*/
 /*console.log('befor timeout console');
 setTimeout(() => console.log('set timeout function called'), 3000);
@@ -55,17 +55,73 @@ console.log('after timeout console');*/
 
 СИНТАКСИС: new Promise((resolve, reject) => {...},
 где resolve, reject - это методы*/
-    let isError = false;
+/*    let isError = false;
 
 const promise = new Promise((resolve, reject) => {
     if (isError) {
-        reject(); //callback, если работа завершилась успешно
+        reject({error: 'New error appeared'}); //callback, если работа завершилась успешно
         //можем в него передать параметры ПР. текст ошибки
     } else {
         resolve( {id: '123'}); //callback, если работа завершилась успешно
         //можем в него передать параметры ПР. данные, которые вернулись с сервера
     }
-})
+})*/
+/////////////////////////////////////////////////
+//Пример короткого промиса
+/*const fulfilledPromise = new Promise(resolve => resolve(42))
+console.log('fulfilledPromise', fulfilledPromise)*/
+
+//////////////////////////////
+ //ДОБАВИМ АССИОНХРОННОСТИ (добавим setTimeout() в промис)
+
+/*const pendingPromise = new Promise(
+    resolve => setTimeout(() => resolve(42),5000)
+)
+console.log('pendingPromise',pendingPromise);*/
+
+//////////////////////////////
+//Раскрытие промисов
+/*fulfilledPromise
+    .then(result => {
+        console.log('first then')
+        return result + 10
+    })
+    .finally(() => console.log('Finally'))
+    .then(result => result > 50 ? console.log('Greater then 50', result): console.log('Less that 50'))*/
+/*
+async await*/
+
+//////////////////////////////
+//Что будет, если применить к промису с ошибкой
+/*const rejectedPromise = new Promise((resolve, reject) =>
+    setTimeout(() => reject('Error 404'), 3000)
+)
+console.log('rejectedPromise', rejectedPromise);
+
+rejectedPromise
+    .then(result => console.log('Result:', result)) //для ситуаций, когда промис сработал успешно
+    .then(null, error => console.log('Then error handing', error))
+    .catch(error => console.log('Catch error handing', error)) //catch предназначен для отлова ошибок
+    //результат catch не выводится, т.к. ошибка уже отловлена предыдущим then
+    .finally(() => console.log('finally'))*/
+//Те методы, которые не работают с ошибкой, не вызываются
+
+//then лучше использовать для успешного промиса,
+// а catch, для неуспешного промиса (ошибки)
+//finally срабатывает всегда, в независимости, был успех или не успех
 
 
-async await
+
+
+/////////////////////////////////////////////////////////
+//запись через стрелочные фун-ции
+/*const someFunction = async () => {*/
+//или такая запись
+async function someFunction() {
+    const promiseResult = await new Promise(resolve => setTimeout(
+        () => resolve('Hello, World'), 3000))
+    return promiseResult;
+}
+
+const functionCallResult = someFunction();
+console.log('functionCallResult',functionCallResult)
