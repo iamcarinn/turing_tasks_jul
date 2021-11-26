afc6a34387d6fd27803a0ca4314e1e398a191a51
iamcarinn <karina825karpenkova@yandex.ru>
2021-11-27 00:25:09 +0300

    17 HW

--- WEEK4/17 (WEEK4DAY1)/HW/9.js
+++ WEEK4/17 (WEEK4DAY1)/HW/9.js
@@ -1,37 +1,24 @@
-let arr1 = [1,2,3,4,5];
-let arr2 = [];
+let arr = [1,2,3,4,5];
 
 // Возвращает массив всех чётных чисел
 const nameFun1 = (array) => {
-    for (let i = 0; i < array.length - 1; i++) {
-        if ((array[i] % 2) === 0) {
-            arr2.push(array[i])
-        }
-    }
-    console.log(arr2);
-    return arr2;
+    return array.filter(item => item % 2 === 0)
 };
 
 //Возвращает сумму чисел массива
 const nameFunc2 = (array) => {
-    let sum = 0;
-    for (let i = 0; i < array.length-1; i++) {
-        sum += array[i]
-    }
-    console.log(sum);
-    return sum;
+    const sum = (firstValue, secondValue) => firstValue + secondValue;
+    return array.reduce(sum);
 } ;
 
 
 //Создаем успешный промис :)
 let promise = new Promise(function (resolve, reject) {
-    setTimeout(() => resolve(arr1), 1000) });
+    setTimeout(() => resolve(arr), 1000) });
 
 //Обрабатываем наш промис с помощью метода then()
+promise.then((array) => nameFun1()).then((array) => nameFunc2());
 
-promise.then((arr1) => nameFun1()).then((arr2) => nameFunc2());
-
-console.log(promise);
 
 
 
