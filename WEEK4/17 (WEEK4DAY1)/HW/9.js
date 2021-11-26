59055d734b190d32ca4220089ee61693fbb5cc90
iamcarinn <karina825karpenkova@yandex.ru>
2021-11-26 23:25:26 +0300

    17.9 HW

--- WEEK4/17 (WEEK4DAY1)/HW/9.js
+++ WEEK4/17 (WEEK4DAY1)/HW/9.js
@@ -1,21 +1,38 @@
 let arr1 = [1,2,3,4,5];
-let arr2 = []
-let sum = 0;
-let promise1 = new Promise(function (resolve) {
-    setTimeout( () => {
-        for (let i = 0; i < arr1.length-1; i++) {
-            if ((arr1[i] % 2) === 0) {
-                arr2.push(arr1[i])
-            }
-        }
-        resolve(`Массив четных чисел [${arr2}]`);
-    }, 1000)
-});
+let arr2 = [];
 
-const promise2 = promise1.then(function () {
-    for (let i = 0; i < arr2.length-1; i++) {
-        sum += arr2[i]
+// Возвращает массив всех чётных чисел
+const nameFun1 = (array) => {
+    for (let i = 0; i < array.length - 1; i++) {
+        if ((array[i] % 2) === 0) {
+            arr2.push(array[i])
+        }
     }
+    console.log(arr2);
+    return arr2;
+};
+
+//Возвращает сумму чисел массива
+const nameFunc2 = (array) => {
+    let sum = 0;
+    for (let i = 0; i < array.length-1; i++) {
+        sum += array[i]
+    }
+    console.log(sum);
     return sum;
-});
+} ;
+
+
+//Создаем успешный промис :)
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(arr1), 1000) });
+
+//Обрабатываем наш промис с помощью метода then()
+
+promise.then((arr1) => nameFun1()).then((arr2) => nameFunc2());
+
+console.log(promise);
+
+
+
 
